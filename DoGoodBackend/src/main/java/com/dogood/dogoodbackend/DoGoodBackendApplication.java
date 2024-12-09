554b4ec17a0be78d0f534c967d0c869e3736092d
EyalShap <eyalm1000@gmail.com>
2024-12-09 21:45:57 +0200

    init

---
+++ DoGoodBackend/src/main/java/com/dogood/dogoodbackend/DoGoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dogood.dogoodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoGoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoGoodBackendApplication.class, args);
+	}
+
+}
---
+++ DoGoodBackend/src/test/java/com/dogood/dogoodbackend/DoGoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogood.dogoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoGoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
